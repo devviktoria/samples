32a5ad587f802cff26cd13a07d707e20639e6dd3
devviktoria <dev.viktoria.simon@gmail.com>
2021-01-18 09:54:41 +0100

    Add error handling to the app

--- mouse_simulator/lib/main.dart
+++ mouse_simulator/lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -6,24 +10,68 @@ import 'background_image_model.dart';
 import 'main_page.dart';
 
 Future<void> main() async {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    debugPrint('Error in the app!!!!!!!!');
+    FlutterError.dumpErrorToConsole(details);
+    exit(1);
+  };
+
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    if (!kReleaseMode) {
+      return Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Text(
+          'Error occured in the Mouse Simulator!\nSorry for the inconvenience.',
+          style: TextStyle(
+            color: Colors.red[900],
+            fontSize: 20.0,
+            decoration: TextDecoration.none,
+          ),
+        ),
+      );
+    }
+
+    return ErrorWidget(details.exception);
+  };
+
+  void _dumpErrorToConsole(Object error, StackTrace? stackTrace) {
+    debugPrint('Error in the app');
+    debugPrint(error.toString());
+    if (stackTrace != null) {
+      debugPrint(stackTrace.toString());
+    } else {
+      debugPrint('No stacktrace');
+    }
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
 
   BackgroundImageModel backgroundImageModel = BackgroundImageModel();
-  await backgroundImageModel.initialize();
+  try {
+    await backgroundImageModel.initialize();
+  } catch (e) {
+    _dumpErrorToConsole(e, null);
+    exit(1);
+  }
 
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => AnimationControllerModel(),
-        ),
-        ChangeNotifierProvider.value(
-          value: backgroundImageModel,
-        )
-      ],
-      child: MyApp(),
-    ),
-  );
+  runZonedGuarded<Future<void>>(() async {
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => AnimationControllerModel(),
+          ),
+          ChangeNotifierProvider.value(
+            value: backgroundImageModel,
+          )
+        ],
+        child: MyApp(),
+      ),
+    );
+  }, (Object error, StackTrace stackTrace) {
+    _dumpErrorToConsole(error, stackTrace);
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -33,15 +81,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mouse Simulator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MainPage(),
