ac2aab814c65012f2d0271c08f149e33f20b32bc
devviktoria <dev.viktoria.simon@gmail.com>
2021-01-19 10:32:57 +0100

    Make app running in portrait only

--- mouse_simulator/lib/main.dart
+++ mouse_simulator/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import 'animation_controller_model.dart';
@@ -46,6 +47,7 @@ Future<void> main() async {
   }
 
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   BackgroundImageModel backgroundImageModel = BackgroundImageModel();
   try {
