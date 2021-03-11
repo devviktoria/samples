a64ab29567e47818de5ef005d03e4fb4b4484fe9
devviktoria <dev.viktoria.simon@gmail.com>
2021-03-11 12:45:35 +0100

    Routing and side menu correction

--- jesterclub/jcangularwebclient/src/app/app.component.ts
+++ jesterclub/jcangularwebclient/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { User } from './models/user';
+import { TempauthService } from './services/tempauth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Jester Club';
+  currentUser?: User = undefined;
+  constructor(private tempauthService: TempauthService) {}
+
+  ngOnInit(): void {
+    this.tempauthService
+      .getLoggedUser()
+      .subscribe((user) => (this.currentUser = user));
+  }
 }
--- jesterclub/jcangularwebclient/src/app/main-page/main-page.component.ts
+++ jesterclub/jcangularwebclient/src/app/main-page/main-page.component.ts
@@ -1,20 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
-import { User } from '../models/user';
-import { TempauthService } from '../services/tempauth.service';
-
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent implements OnInit {
-  currentUser?: User = undefined;
-  constructor(private tempauthService: TempauthService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.tempauthService
-      .getLoggedUser()
-      .subscribe((user) => (this.currentUser = user));
-  }
+  ngOnInit(): void {}
 }
