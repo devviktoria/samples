10c9fb60e82a9d53d1539af4936f84b98d763dd7
devviktoria <dev.viktoria.simon@gmail.com>
2021-03-21 10:35:49 +0100

    Added UpdateEmotionCounter stored procedure

--- jesterclub/jcwebapisqlserver/Migrations/20210321092253_UpdateEmotionCounterSP.Designer.cs
+++ jesterclub/jcwebapisqlserver/Migrations/20210321092253_UpdateEmotionCounterSP.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using jcwebapi.Repository;
 
 namespace jcwebapisqlserver.Migrations
 {
     [DbContext(typeof(SqlServerDbContext))]
-    partial class SqlServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210321092253_UpdateEmotionCounterSP")]
+    partial class UpdateEmotionCounterSP
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ jesterclub/jcwebapisqlserver/Migrations/20210321092253_UpdateEmotionCounterSP.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace jcwebapisqlserver.Migrations {
+    public partial class UpdateEmotionCounterSP : Migration {
+        protected override void Up (MigrationBuilder migrationBuilder) {
+            var spSql = @"CREATE PROCEDURE dbo.UpdateEmotionCounters(@jokeId int, @emotion nvarchar(50))
+AS
+
+IF (NOT EXISTS (SELECT * FROM dbo.Joke WHERE JokeId = @jokeId))
+BEGIN 
+    THROW 51000, 'The joke with the given id does not exist.', 1;  
+END
+
+BEGIN TRANSACTION;
+UPDATE dbo.EmotionCounter
+SET Counter = Counter + 1
+WHERE JokeId = @jokeId AND Emotion = @emotion 
+
+DECLARE @currentTime AS datetimeoffset = SYSDATETIMEOFFSET();
+SET DATEFIRST 1;
+DECLARE @weekDay AS int = DATEPART(weekday, @currentTime);
+
+UPDATE dbo.ResponseStatistic
+SET Counter = Counter + 1
+WHERE JokeId = @jokeId AND [Day] = @weekDay
+
+UPDATE dbo.Joke 
+SET ResponseSum = ResponseSum + 1
+WHERE JokeId = @jokeId
+COMMIT;";
+            migrationBuilder.Sql (spSql);
+        }
+
+        protected override void Down (MigrationBuilder migrationBuilder) {
+            var spSql = "DROP PROCEDURE IF EXISTS dbo.UpdateEmotionCounters;";
+            migrationBuilder.Sql (spSql);
+        }
+    }
+}
